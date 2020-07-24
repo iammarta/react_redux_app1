742841731ac79d3cb2d263621032b84f8d22ad18
Marta Kozak <51427243+iammarta@users.noreply.github.com>
2020-07-24 13:39:32 +0300

    upload build folder

---
+++ build/precache-manifest.4516c2356e1217d0e3bede2bad3088b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdbc581f074d82197234227d46203aee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56c5ee7438d5593978cf",
+    "url": "/static/css/main.8ebd21bc.chunk.css"
+  },
+  {
+    "revision": "1b6d94744b835a5afe76",
+    "url": "/static/js/2.5bb5f9fc.chunk.js"
+  },
+  {
+    "revision": "56c5ee7438d5593978cf",
+    "url": "/static/js/main.80e65fc4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "698e5e807daf0026f6ac590ce288822c",
+    "url": "/static/media/food-bg.698e5e80.jpg"
+  },
+  {
+    "revision": "701d6d1d57f5d528c4db1e887ff29e82",
+    "url": "/static/media/shopping-cart-solid.701d6d1d.svg"
+  }
+]);
